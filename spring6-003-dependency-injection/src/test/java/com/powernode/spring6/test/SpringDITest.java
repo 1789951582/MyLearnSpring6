ad8e1e11c5bc1b122dbf609ce3991e5f364d04b1
小皓 <cfis@foxmail.com>
2025-10-12 23:05:06 +0800

    P018_依赖注入之构造注入

--- spring6-003-dependency-injection/src/test/java/com/powernode/spring6/test/SpringDITest.java
+++ spring6-003-dependency-injection/src/test/java/com/powernode/spring6/test/SpringDITest.java
@@ -1,11 +1,25 @@
 package com.powernode.spring6.test;
 
+import com.powernode.spring6.service.CustomerService;
 import com.powernode.spring6.service.UserService;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class SpringDITest {
+
+    @Test
+    public void testConstructorDI(){
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("beans.xml");
+        CustomerService csBean = applicationContext.getBean("csBean", CustomerService.class);
+        csBean.save();
+
+        CustomerService csBean2 = applicationContext.getBean("csBean2", CustomerService.class);
+        csBean2.save();
+
+        CustomerService csBean3 = applicationContext.getBean("csBean3", CustomerService.class);
+        csBean3.save();
+    }
 
     @Test
     public void testSetDI(){
