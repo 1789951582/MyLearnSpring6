29cc85d85794cba6ca5ffac9865b92b095fdca2c
小皓 <cfis@foxmail.com>
2025-10-12 22:45:30 +0800

    P017_依赖注入之set注入

---
+++ spring6-003-dependency-injection/src/test/java/com/powernode/spring6/test/SpringDITest.java
@@ -0,0 +1,17 @@
+package com.powernode.spring6.test;
+
+import com.powernode.spring6.service.UserService;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class SpringDITest {
+
+    @Test
+    public void testSetDI(){
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("spring.xml");
+        UserService userServiceBean = applicationContext.getBean("userServiceBean", UserService.class);
+        userServiceBean.saveUser();
+
+    }
+}
