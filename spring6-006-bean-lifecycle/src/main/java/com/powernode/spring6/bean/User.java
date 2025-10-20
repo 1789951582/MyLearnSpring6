f95c1eaa24af7329735f97d5159bdfb5df1110be
小皓 <cfis@foxmail.com>
2025-10-20 23:15:50 +0800

    P049_Bean的生命周期之五步

---
+++ spring6-006-bean-lifecycle/src/main/java/com/powernode/spring6/bean/User.java
@@ -0,0 +1,32 @@
+package com.powernode.spring6.bean;
+
+/**
+ * Bean的生命周期按照粗略的五步的话：
+ * 第一步：实例化Bean（调用无参数构造方法）
+ * 第二步：给Bean属性赋值（调用set方法）
+ * 第三步：初始化Bean（会调用Bean的init方法。注意：这个init方法需要自己写，自己配。）
+ * 第四步：使用Bean
+ * 第五步：销毁Bean（会调用Bean的destroy方法。注意：这个destory方法需要自己写。自己配。）
+ */
+public class User {
+    private String name;
+
+    public void setName(String name) {
+        System.out.println("第二步：给对象的属性赋值。");
+        this.name = name;
+    }
+
+    public User() {
+        System.out.println("第一步，无参数构造方法执行");
+    }
+
+    //这个方法需要自己写，自己配，方法名随意
+    public void initBean(){
+        System.out.println("第三步：初始化Bean");
+    }
+
+    //这个方法需要自己写。自己配。方法名随意。
+    public void destroyBean(){
+        System.out.println("第五步：销毁Bean");
+    }
+}
---
+++ spring6-006-bean-lifecycle/src/test/java/com/powernode/spring6/BeanLifecycleTest.java
@@ -0,0 +1,19 @@
+package com.powernode.spring6;
+
+import com.powernode.spring6.bean.User;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class BeanLifecycleTest {
+
+    @Test
+    public void testBeanLifecycleFive(){
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("spring.xml");
+        User user = applicationContext.getBean("user", User.class);
+        System.out.println("第四步：使用Bean：" + user);
+        //必须手动关闭Spring容器，这个Spring容器才会销毁Bean
+        ClassPathXmlApplicationContext context = (ClassPathXmlApplicationContext) applicationContext;
+        context.close();
+    }
+}
