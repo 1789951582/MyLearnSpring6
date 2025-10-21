ae8cc0580cf8f57660a653eb93616b22ea205919
小皓 <cfis@foxmail.com>
2025-10-21 21:38:42 +0800

    P053_自己new的对象纳入Spring容器管理

--- spring6-006-bean-lifecycle/src/test/java/com/powernode/spring6/BeanLifecycleTest.java
+++ spring6-006-bean-lifecycle/src/test/java/com/powernode/spring6/BeanLifecycleTest.java
@@ -1,11 +1,28 @@
 package com.powernode.spring6;
 
+import com.powernode.spring6.bean.Student;
 import com.powernode.spring6.bean.User;
 import org.junit.Test;
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class BeanLifecycleTest {
+
+    @Test
+    public void testRegisterBean(){
+        //自己new的对象
+        Student student = new Student();
+        System.out.println(student);
+
+        //将以上自己new的这个对象纳入到Spring容器来管理。半路上交给Spring来管理
+        DefaultListableBeanFactory factory = new DefaultListableBeanFactory();
+        factory.registerSingleton("studentBean", student);
+
+        //从spring容器中获取
+        Student studentBean = factory.getBean("studentBean", Student.class);
+        System.out.println(studentBean);
+    }
 
     /**
      * Spring容器只对singleton的Bean进行完整的生命周期管理。
