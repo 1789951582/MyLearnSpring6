fe35c1760299049f5cab9b53e62d5aa16ae8408c
小皓 <cfis@foxmail.com>
2025-10-22 23:11:36 +0800

    P061_回顾反射机制之SpringDI核心实现

---
+++ review-reflect/src/main/java/com/powernode/reflect/Test4.java
@@ -0,0 +1,42 @@
+package com.powernode.reflect;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
+public class Test4 {
+    public static void main(String[] args) throws Exception{
+        /**
+         * 需求：
+         *      假设你现在已知以下信息：
+         *          1.有这样一个类，类名叫做：com.powernode.reflect.User
+         *          2.这个类符合javabean规范。属性私有化，对外提供setter和getter方法。
+         *          3.你还知道这个类当中有一个属性，属性的名字叫做age
+         *          4。并且你还知道age属性类型是int类型
+         *      请使用反射机制调用set方法，给User对象的age属性赋值
+         */
+        String className = "com.powernode.reflect.User";
+        String propertyName = "age";
+
+        //通过反射机制调用setAge(int)方法
+        //获取类
+        Class<?> clazz = Class.forName(className);
+
+//        获取方法名
+        String setMethodName = "set" + propertyName.toUpperCase().charAt(0) + propertyName.substring(1);
+
+        //根据属性名获取属性类型
+        Field declaredField = clazz.getDeclaredField(propertyName);
+        Class<?> type = declaredField.getType();
+
+        //获取方法
+        Method setMethod = clazz.getDeclaredMethod(setMethodName, type);
+
+        //准备对象
+        Object obj = clazz.newInstance();
+
+        //调用方法
+        setMethod.invoke(obj,30);
+
+        System.out.println(obj);
+    }
+}
