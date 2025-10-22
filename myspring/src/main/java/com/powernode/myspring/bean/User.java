a6f1270942bf0c6a2f5e851b5377e83dd52038ce
小皓 <cfis@foxmail.com>
2025-10-22 23:29:38 +0800

    P062_手写Spring框架之准备工作

---
+++ myspring/src/main/java/com/powernode/myspring/bean/User.java
@@ -0,0 +1,23 @@
+package com.powernode.myspring.bean;
+
+public class User {
+    private String name;
+
+    private int age;
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
