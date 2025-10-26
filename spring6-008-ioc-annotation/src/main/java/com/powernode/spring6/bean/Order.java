852d0bea99f868456c34c6676bb1146d24de0e2c
小皓 <cfis@foxmail.com>
2025-10-26 11:43:08 +0800

    P073_Spring IoC注解之Spring注解的使用

---
+++ spring6-008-ioc-annotation/src/main/java/com/powernode/spring6/bean/Order.java
@@ -0,0 +1,10 @@
+package com.powernode.spring6.bean;
+
+import org.springframework.stereotype.Service;
+
+@Service //如果你把整个value属性全部省略了，bean有没有默认的名称？有： 类名首字母变小写就是bean的名字
+public class Order {
+}
+
+//以上这个注解@Service就相当于以下的这个配置信息
+//<bean id="order" class="com.powernode.spring6.bean.Order"></bean>
---
+++ spring6-008-ioc-annotation/src/test/java/com/powernode/spring6/test/IoCAnnotationTest.java
@@ -0,0 +1,25 @@
+package com.powernode.spring6.test;
+
+import com.powernode.spring6.bean.*;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class IoCAnnotationTest {
+
+    @Test
+    public void testBeanComponent(){
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("spring.xml");
+        User userBean = applicationContext.getBean("userBean", User.class);
+        System.out.println(userBean);
+        Vip vipBean = applicationContext.getBean("vipBean", Vip.class);
+        System.out.println(vipBean);
+        Order orderBean = applicationContext.getBean("order", Order.class);
+        System.out.println(orderBean);
+        Student studentBean = applicationContext.getBean("student", Student.class);
+        System.out.println(studentBean);
+
+        OrderService orderService = applicationContext.getBean("orderService", OrderService.class);
+        System.out.println(orderService);
+    }
+}
