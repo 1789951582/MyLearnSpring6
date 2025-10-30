f3316ea91c8fb8152da982b124d49cb89aa81f79
小皓 <cfis@foxmail.com>
2025-10-30 22:36:09 +0800

    P082_JdbcTemplate之新增

--- spring6-009-jdbc/src/test/java/com/powernode/spring6/test/SpringJdbcTest.java
+++ spring6-009-jdbc/src/test/java/com/powernode/spring6/test/SpringJdbcTest.java
@@ -6,6 +6,18 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 public class SpringJdbcTest {
+
+    @Test
+    public void testInsert(){
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("spring.xml");
+        JdbcTemplate jdbcTemplate = applicationContext.getBean("jdbcTemplate", JdbcTemplate.class);
+        // insert语句
+        String sql = "insert into t_user(real_name, age) values(?,?)";
+        // 注意：在JdbcTemplate当中，只要insert delete update语句的，都是调用update方法。
+        int count = jdbcTemplate.update(sql, "王五", 20);
+        System.out.println(count);
+    }
+
     @Test
     public void testJdbc(){
         ApplicationContext applicationContext = new ClassPathXmlApplicationContext("spring.xml");
